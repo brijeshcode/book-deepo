46917fb90b8bf678a96bfc4c011c8a849495c65b
brijeshcode <brieshchaturvedi.it@gmail.com>
2022-01-24 10:03:12 +0530

    bug fix

--- app/Http/Controllers/Order/SupplierDeliveryController.php
+++ app/Http/Controllers/Order/SupplierDeliveryController.php
@@ -53,8 +53,10 @@ class SupplierDeliveryController extends Controller
             // 1.& 2  Insert supplier delivery &&  insert supplier delivery items
             $order = SupplierOrderDelivery::create($request->only('date', 'supplier_id', 'school_id', 'supplier_order_id', 'school_order_id',  'quantity', 'discount_percent', 'discount', 'sub_total', 'total_amount','note'))->items()->createMany($request->items);
 
+
             $schoolOrder = SchoolOrder::whereId($request->school_order_id)->first();
             $schoolOrder->status = $schoolOrder->quantity ==  $order->quantity  ? 'Completed' : 'Partial';
+
             $schoolOrder->save();
 
             foreach ($request->items as $key => $item) {
